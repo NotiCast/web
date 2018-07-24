00d49ddaa1ac9be992fa51a51f4506bdd28f2bb0
RyanSquared <vandor2012@gmail.com>
2018-07-24 14:08:20 -0500

    setup.py: Remove, as noticast requires a different build process

--- setup.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=missing-docstring
-from distutils.core import setup
-
-setup(
-    name='noticast_web',
-    version='0.1-dev',
-    packages=['noticast_web'],
-    include_package_data=True,
-    install_requires=['flask', 'flask-sqlalchemy'])
