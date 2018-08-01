11c1d6fa5c1bd0498e1f823ea3e996f9581fdfa6
RyanSquared <vandor2012@gmail.com>
2018-08-01 16:05:21 -0500

    setup.py: Include data files

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
     name='noticast_web',
     version='0.1-dev',
     packages=['noticast_web'],
-    install_requires=['flask', 'flask_sqlalchemy'])
+    package_data={
+        "noticast_web": ["templates/*", "static/*"]
+    },
+    install_requires=['flask', 'flask_sqlalchemy', 'spudbucket',
+                      'boto3', 'psycopg2-binary', 'pymysql'])
