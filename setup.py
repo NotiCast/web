34863b157ef6f94767072407b40d2202086e6cf6
RyanSquared <vandor2012@gmail.com>
2018-08-01 15:36:29 -0500

    setup.py: Initial Commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+# pylint: disable=missing-docstring
+from distutils.core import setup
+
+setup(
+    name='noticast_web',
+    version='0.1-dev',
+    packages=['noticast_web'],
+    install_requires=['flask', 'flask_sqlalchemy'])
