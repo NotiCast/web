da6c4359f305c01bed3821f1c8a00ca6b28770ce
RyanSquared <vandor2012@gmail.com>
2018-08-02 14:43:53 -0500

    noticast_web/models: Move models to external declarative base

--- noticast_web/models.py
+++ noticast_web/models.py
@@ -4,55 +4,9 @@ from flask.cli import with_appcontext
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError  # noqa: F401
 
-db = SQLAlchemy()
+from .rds_models import Base, User, Client, Device, Group  # noqa: F401
 
-
-class Client(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    is_admin = db.Column(db.Boolean, nullable=False, default=False)
-    users = db.relationship("User", backref="client", lazy=True)
-    groups = db.relationship("Group", backref="client", lazy=True)
-    devices = db.relationship("Device", backref="client", lazy=True)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    client_id = db.Column(db.Integer, db.ForeignKey("client.id"),
-                          nullable=False)
-    username = db.Column(db.String(64), nullable=False, unique=True)
-    password = db.Column(db.String(256), nullable=False)
-
-
-devices2groups = db.Table(
-    "devices2groups",
-    db.Column("device_id", db.Integer, db.ForeignKey("device.id"),
-              primary_key=True, nullable=False),
-    db.Column("group_id", db.Integer, db.ForeignKey("group.id"),
-              primary_key=True, nullable=False)
-)
-
-
-class Device(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    arn = db.Column(db.String(256), unique=True, nullable=False)
-    name = db.Column(db.String(64), unique=False, nullable=False)
-    client_id = db.Column(db.Integer, db.ForeignKey("client.id"),
-                          nullable=False)
-    __table_args__ = (db.UniqueConstraint("client_id", "name",
-                                          name="_unique_group_name"),)
-
-
-class Group(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    client_id = db.Column(db.Integer, db.ForeignKey("client.id"),
-                          nullable=False)
-    arn = db.Column(db.String(256), nullable=False, unique=True)
-    name = db.Column(db.String(64), nullable=False)
-    devices = db.relationship("Device", secondary=devices2groups,
-                              lazy="subquery", backref=db.backref("groups"))
-    __table_args__ = (db.UniqueConstraint("client_id", "name",
-                                          name="_unique_group_name"),)
-    # devices exists in the `groups` backref
+db = SQLAlchemy(model_class=Base)
 
 
 @click.command("init-db")
