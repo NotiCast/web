e12435dab13dd0f75465d6db252ca2da660db0ac
RyanSquared <vandor2012@gmail.com>
2018-08-02 07:57:08 -0500

    setup.py: Recursively include data files

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 # pylint: disable=missing-docstring
 from distutils.core import setup
 
+import os
+
+files = []
+for directory in ["noticast_web/static", "noticast_web/templates"]:
+    for path, dirs, filenames in os.walk(directory):
+        files.append(os.path.join("..", path, "*"))
+
+print(files)
+
 setup(
     name='noticast_web',
     version='0.1-dev',
     packages=['noticast_web'],
     package_data={
-        "noticast_web": ["templates/*", "static/*"]
+        "noticast_web": files
     },
     install_requires=['flask', 'flask_sqlalchemy', 'spudbucket',
                       'boto3', 'psycopg2-binary', 'pymysql'])
